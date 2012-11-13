e12d2fed9ca042eb10af063d8a77fc70b3923d11
onema <kinojman@gmail.com>
2012-11-13 15:47:55 -0800

    I was having issue using the collection class as a query builder because fuel was looking the JSON class in the same namespance. I've added the name space to the json class and added the class name and path to the bootstrap. Query builder now working as expected.

--- bootstrap.php
+++ bootstrap.php
@@ -14,4 +14,6 @@ Autoloader::add_classes(array(
 	'Mongo_Odm\\Collection'	=> __DIR__.'/classes/collection.php',
 	'Mongo_Odm\\Database' 	=> __DIR__.'/classes/database.php',
 	'Mongo_Odm\\Document' 	=> __DIR__.'/classes/document.php',
+	'Mongo_Odm\\Document' 	=> __DIR__.'/classes/document.php',
+	'Mongo_Odm\\JSON'       => __DIR__.'/classes/json.php',
 ));
